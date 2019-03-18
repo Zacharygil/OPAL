5113c205c68f50743fb2987b76972c7c340054d8
Zacharygil <Zaculagil@gmail.com>
2019-03-18 18:21:20 -0500

    intial commit

---
+++ OPAL/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  OPAL
+//
+//  Created by Zachary Gilchrist on 3/10/19.
+//  Copyright © 2019 Zachary Gilchrist. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OPAL/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  OPAL
+//
+//  Created by Zachary Gilchrist on 3/10/19.
+//  Copyright © 2019 Zachary Gilchrist. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
